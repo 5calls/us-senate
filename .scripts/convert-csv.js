eedc9e384710fa2a969f017a7f6f55f6155542d0
Nick O'Neill <nick.oneill@gmail.com>
2019-05-21 16:55:24 -0700

    Updated data for new image endpoints.

--- .scripts/convert-csv.js
+++ .scripts/convert-csv.js
@@ -100,7 +100,7 @@ if (fs.existsSync(core)) {
       })
       .on('data', function(data){
 
-        var cdnHeadshotPath = (data.vacant === 'yes') ? '' : 'https://cdn.civil.services/us-senate/headshots/512x512/' +
+        var cdnHeadshotPath = (data.vacant === 'yes') ? '' : 'https://repimages.5calls.org/us-senate/512x512/' +
           slug(data.first_name + ' ' + data.last_name, { lower: true, replacement: '-' }) + '.jpg';
 
         var parsedAddress = data.address.split(',');
